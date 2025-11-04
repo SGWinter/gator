07bb6df9f2bc89224071af41ccb6e992d2ddb4f5
SGWinter <seangcalgary@gmail.com>
2025-11-04 08:18:47 -0700

    db reset

--- handler_user.go
+++ handler_user.go
@@ -9,6 +9,20 @@ import (
 	"github.com/google/uuid"
 )
 
+func handlerReset(s *state, cmd command) error {
+	if len(cmd.Args) != 0 {
+		return fmt.Errorf("usage: %v", cmd.Name)
+	}
+
+	err := s.db.DeleteAllUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete all users: %w", err)
+	}
+	fmt.Println("Database reset successfully!")
+
+	return nil
+}
+
 func handlerRegister(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("usage: %v <name>", cmd.Name)
